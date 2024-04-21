053009c806e2067c2a31354f26b02bf42c9f6a11
natthasath <natthasath.sak@gmail.com>
2024-04-21 10:15:17 +0700

    Change Email User

--- app/Http/Controllers/SendmailController.php
+++ app/Http/Controllers/SendmailController.php
@@ -10,7 +10,7 @@ class SendmailController extends Controller
 {
     public function sendmail()
     {
-        $email = "natthasath.sak@nida.ac.th";
+        $email = "user@example.com";
         Mail::to($email)->send(new WelcomeEmail());
         return True;
         // Read - Display a list of items
